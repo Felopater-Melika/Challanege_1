cb013e1a2cc3255ff499ca72db05992c5a33adfb
Felopater-Melika <felopatermelika@gmail.com>
2025-01-25 19:07:53 -0600

    fmt

--- flake.nix
+++ flake.nix
@@ -5,19 +5,21 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
     devShells.x86_64-linux.default = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in pkgs.mkShell {
-      name = "Challenge 1";
-
-      buildInputs = [
-        (pkgs.python312.withPackages (ps: [
-          ps.requests
-          ps.python-dotenv
-        ]))
-      ];
-
-    };
+    in
+      pkgs.mkShell {
+        name = "Challenge 1";
+        buildInputs = [
+          (pkgs.python312.withPackages (ps: [
+            ps.requests
+            ps.python-dotenv
+          ]))
+        ];
+      };
   };
 }
