cbd25b52286bfb6942b5359612a90c99107c7a67
Felopater-Melika <felopatermelika@gmail.com>
2025-02-07 15:04:53 -0600

    Challenge_2

---
+++ 2/Challenge_2/main.py
@@ -0,0 +1,18 @@
+import re
+
+with open('text.txt', 'r') as file:
+    text = file.read()
+
+phone_pattern = re.compile(r'\(?\d{3}\)?[ -.]?\d{3}[ -.]?\d{4}')
+email_pattern = re.compile(r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}')
+
+phones = phone_pattern.findall(text)
+emails = email_pattern.findall(text)
+
+print("Phone Numbers:")
+for phone in phones:
+    print(phone)
+
+print("\nEmail Addresses:")
+for email in emails:
+    print(email)
